36630f00bedfdeaa4b0033bc63e51110225497e1
davidbegin <davidmichaelbe@gmail.com>
2019-10-07 18:21:18 -0700

    Fix the New God checking Logic

--- lib/status_checkers.py
+++ lib/status_checkers.py
@@ -5,11 +5,12 @@ from lib.morgue_parser import fetch_altars
 def check_for_new_gods(old_altars, morgue_file, printer):
     altars = set(fetch_altars(morgue_file))
 
-    if old_altars is not None:
-        if len(altars) > len(old_altars):
-            new_altars = altars.difference(set(old_altars))
+    if old_altars and len(altars) > len(old_altars):
+        new_altars = altars.difference(set(old_altars))
+        # TODO: make this message work for 1 or more Altars
+        print("We Found a new God: {new_altars}")
+        printer.print_missionary(new_altars)
+    else:
+        new_altars = {}
+    return altars, new_altars
 
-            # TODO: make this message work for 1 or more Altars
-            print("We Found a new God: {new_altars}")
-            printer.print_missionary(new_altars)
-    return new_altars
--- test/test_status_checkers.py
+++ test/test_status_checkers.py
@@ -27,7 +27,7 @@ def test_check_for_new_gods():
     }
     old_altars = {"Xom"}
     morgue_file = open("Support/WilliamGates.txt").read()
-    new_altars = check_for_new_gods(old_altars, morgue_file, FakePrinter())
+    all_altars, new_altars = check_for_new_gods(old_altars, morgue_file, FakePrinter())
     # import code; code.interact(local=dict(globals(), **locals()))
     assert set(new_altars) == current_altars.difference(old_altars)
 
